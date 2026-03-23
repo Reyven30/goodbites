247b213474e9b17f9076e2e96915e314d94d0877
Cris Reyven Flores <crisreyven.flores@iticopernico.it>
2026-03-23 09:27:45 +0100

    Creates connection to database

--- data/db.php
+++ data/db.php
@@ -1,10 +1,9 @@
 <?php
 $conn = new mysqli("localhost", "flores", "vWy7Xr7kvdpp", "my_flores");
-$adminHash = password_hash("adminpass", PASSWORD_DEFAULT);
-$userHash = password_hash("userpass", PASSWORD_DEFAULT);
 
-$conn->query("INSERT INTO users (name,email,password) VALUES ('Admin', 'admin@goodbites.com', '$adminHash')");
-$conn->query("INSERT INTO users (name,email,password) VALUES ('user', 'user@goodbites.com', '$userHash')");
-if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);// stampa l’errore di connessione
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-?>  
+$conn->set_charset("utf8mb4");
+?>
